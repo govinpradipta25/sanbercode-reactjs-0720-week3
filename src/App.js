f9e481678ac1881d6c10aa6b4107ea77a9f4b7b3
govin <govinyudian.p@gmail.com>
2020-08-04 20:49:21 +0700

    menambahkan tugas 11(ngulang commit karna belum masuk datanya)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Table from './tugas11/tabelbuah';
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App
